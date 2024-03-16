17768952732ca54910a163605af1e0f443274726
N Tanaka <66735232+NTanaka1994@users.noreply.github.com>
2024-03-16 20:55:04 +0900

    見やすく

--- 翻訳/transrate.py
+++ 翻訳/transrate.py
@@ -2,20 +2,20 @@ from googletrans import Translator
 import glob
 import os
 
-trans=Translator(service_urls=['translate.googleapis.com'])
+trans = Translator(service_urls=['translate.googleapis.com'])
 
-files=glob.glob("english/*")
+files = glob.glob("english/*")
 
 for file in files:
-    f=open(file,"r",encoding="utf-8")
-    text=f.read()
+    f = open(file, "r", encoding="utf-8")
+    text = f.read()
     f.close()
     while True:
         try:
-            transed=trans.translate(text,dest="ja")
+            transed = trans.translate(text, dest="ja")
             break
         except Exception as e:
-            trans=Translator(service_urls=['translate.googleapis.com'])
-    f=open("japanese/"+os.path.split(file)[1],"w",encoding="utf-8")
+            trans = Translator(service_urls=['translate.googleapis.com'])
+    f = open("japanese/"+os.path.split(file)[1], "w", encoding="utf-8")
     f.write(transed.text)
     f.close()
