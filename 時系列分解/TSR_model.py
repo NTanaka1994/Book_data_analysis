9b652c22fb07fcd97f923f22ae4bd58077c3d3f0
N Tanaka <66735232+NTanaka1994@users.noreply.github.com>
2024-03-16 20:11:04 +0900

    見やすく

--- 時系列分解/TSR_model.py
+++ 時系列分解/TSR_model.py
@@ -2,14 +2,14 @@
 from statsmodels import api as sm
 import pandas as pd
 import matplotlib.pyplot as plt
-df=pd.read_csv("chingin.csv",encoding="shift-jis")
-TSR=sm.tsa.seasonal_decompose(df["賃金指数"].values, period=6)
+df = pd.read_csv("chingin.csv", encoding="shift-jis")
+TSR = sm.tsa.seasonal_decompose(df["賃金指数"].values, period=6)
 #TSR=sm.tsa.seasonal_decompose(df["感染者数"].values, freq=7)
 TSR.plot()
 plt.show()
-trend=TSR.trend
-seasonal=TSR.seasonal
-residual=TSR.resid
+trend = TSR.trend
+seasonal = TSR.seasonal
+residual = TSR.resid
 plt.plot(trend)
 plt.show()
 plt.plot(seasonal)
