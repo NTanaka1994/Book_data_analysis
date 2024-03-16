7df7d1791686578dea8909d0814aa4b5ae640ffd
N Tanaka <66735232+NTanaka1994@users.noreply.github.com>
2024-03-16 18:14:11 +0900

    Update cox_hazard.py

--- Coxハザード/cox_hazard.py
+++ Coxハザード/cox_hazard.py
@@ -2,14 +2,14 @@ from lifelines import CoxPHFitter
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df=pd.read_csv("rossi.csv",encoding="shift-jis")
+df = pd.read_csv("rossi.csv",encoding="shift-jis")
 
-model=CoxPHFitter()
-model.fit(df,duration_col="week",event_col="arrest")
+model = CoxPHFitter()
+model.fit(df,duration_col="week", event_col="arrest")
 
 model.print_summary()
 
-pred=model.predict_survival_function(df)
+pred = model.predict_survival_function(df)
 plt.plot(pred[0].values)#1人目のデータ
 plt.plot(pred[1].values)#2人目のデータ
 plt.show()
