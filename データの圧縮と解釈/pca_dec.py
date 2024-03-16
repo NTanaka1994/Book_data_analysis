ae9de7a9186fb34dfddbf2b7fc9e8deb5e9df086
N Tanaka <66735232+NTanaka1994@users.noreply.github.com>
2024-03-16 18:27:56 +0900

    見やすく

--- データの圧縮と解釈/pca_dec.py
+++ データの圧縮と解釈/pca_dec.py
@@ -3,37 +3,37 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-df=pd.read_csv("wine.csv")
-y_name="Wine"
-y=df[y_name].values
-x_tab=df.drop(y_name,axis=1)
-x_name=x_tab.columns
-x=x_tab.values
+df = pd.read_csv("wine.csv")
+y_name = "Wine"
+y = df[y_name].values
+x_tab = df.drop(y_name, axis=1)
+x_name = x_tab.columns
+x = x_tab.values
 
-model=PCA()
+model = PCA()
 model.fit(x)
-con=model.explained_variance_ratio_
-fac=model.components_
-dff=pd.DataFrame(fac)
-dfc=pd.DataFrame(con)
-dff.columns=x_name
-index=[]
+con = model.explained_variance_ratio_
+fac = model.components_
+dff = pd.DataFrame(fac)
+dfc = pd.DataFrame(con)
+dff.columns = x_name
+index = []
 for i in range(len(fac)):
     index.append("第%d主成分"%(i+1))
-dff.index=index
-dfc.columns=["寄与率"]
-dfc.index=index
-dfcc=dfc.cumsum()
-dfcc.columns=["累積寄与率"]
-dff=pd.concat([dff,dfc,dfcc],axis=1)
+dff.index = index
+dfc.columns = ["寄与率"]
+dfc.index = index
+dfcc = dfc.cumsum()
+dfcc.columns = ["累積寄与率"]
+dff = pd.concat([dff, dfc, dfcc], axis=1)
 
-factor=[]
+factor = []
 for i in range(len(fac)):
     factor.append(np.sqrt(con[i])*fac[i])
-dffac=pd.DataFrame(factor)
-dffac.columns=x_name
-dffac.index=index
+dffac = pd.DataFrame(factor)
+dffac.columns = x_name
+dffac.index = index
 
-tx=model.transform(x)
-plt.scatter(tx[:,0],tx[:,1],c=y,cmap="brg")
+tx = model.transform(x)
+plt.scatter(tx[:, 0], tx[:, 1], c=y, cmap="brg")
 plt.show()
