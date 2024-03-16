3ebf5ade79160b877c486c5f865649ab811fb014
N Tanaka <66735232+NTanaka1994@users.noreply.github.com>
2024-03-16 18:39:23 +0900

    見やすく

--- ベイジアンネットワーク/factor_analysis.py
+++ ベイジアンネットワーク/factor_analysis.py
@@ -7,13 +7,13 @@ import matplotlib.pyplot as plt
 import os
 import networkx as nx
 
-df=pd.read_csv("breast_cancer.csv")
+df = pd.read_csv("breast_cancer.csv")
 
-SM=from_pandas(df)
+SM = from_pandas(df)
 
-pos=nx.spring_layout(SM,k=60)
+pos = nx.spring_layout(SM, k=60)
 nx.draw_network_labels(SM)
-edge_width=[ d['weight']*0.3 for (u,v,d) in SM.edges(data=True)]
+edge_width = [d['weight'] * 0.3 for (u, v, d) in SM.edges(data=True)]
 nx.draw_networkx_labels(SM, pos)
-nx.draw_networkx(SM,pos,node_size=4000,arrowsize=20,alpha=1.0,edge_color='b',width=edge_width)
+nx.draw_networkx(SM, pos, node_size=4000, arrowsize=20, alpha=1.0, edge_color='b', width=edge_width)
 plt.show()
