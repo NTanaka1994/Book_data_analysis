e5f24327df4a48238c5a6d506e70d341b6469ee1
N Tanaka <66735232+NTanaka1994@users.noreply.github.com>
2024-03-16 21:02:41 +0900

    見やすく

--- 類似データ/k_means.py
+++ 類似データ/k_means.py
@@ -3,17 +3,17 @@ from sklearn.cluster import KMeans
 import pandas as pd
 import numpy as np
 
-df=pd.read_csv("wine.csv")
-x_tab=df.drop("category",axis=1)
-x=x_tab.values
-x_mm=preprocessing.minmax_scale(x)
-model=KMeans(n_clusters=3)
+df = pd.read_csv("wine.csv")
+x_tab = df.drop("category",axis=1)
+x = x_tab.values
+x_mm = preprocessing.minmax_scale(x)
+model = KMeans(n_clusters=3)
 model.fit(x_mm)
-pred=model.predict(x_mm)
-col=list(x_tab.columns)
+pred = model.predict(x_mm)
+col = list(x_tab.columns)
 col.append("category")
-pred=pred.reshape(-1,1)
-data=np.hstack((x,pred))
-df_out=pd.DataFrame(data)
-df_out.columns=col
-df_out.to_csv("wine_cluster.csv",index=False)
+pred=pred.reshape(-1, 1)
+data = np.hstack((x, pred))
+df_out = pd.DataFrame(data)
+df_out.columns = col
+df_out.to_csv("wine_cluster.csv", index=False)
